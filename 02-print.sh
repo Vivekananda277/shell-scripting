6cfec284c4a9ee0b9c9ed38b23526096d96b1fd9
Vivek <vivekanandareddy7677@gmail.com>
2021-07-17 23:48:40 +0530

    Printing multiple lines in different colours

--- 02-print.sh
+++ 02-print.sh
@@ -5,3 +5,23 @@ echo Hello Bhai
 #print multiple lines
 
 echo -e Hello Bhai,\n\t Namasthe
+
+#Enabling the colours(\e)
+
+##Synatx : echo -e "\eColmMessage"
+#COLOURS AND THEIR CODES
+#RED 31
+
+#GREEN 32
+
+#YELLOW 33
+
+#BLUE  34
+
+#MAGENTA 35
+
+#CYAN 36
+
+echo -e "\e[31mMESSAGE IN RED"
+
+echo -e "\e[33mMESSAGE IN YELLOW
