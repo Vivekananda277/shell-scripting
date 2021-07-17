43e7364b0dcffcbaa9e674ab3940824ff087f36d
Vivek <vivekanandareddy7677@gmail.com>
2021-07-17 23:59:44 +0530

    Printing multiple lines in different colours

--- 02-print.sh
+++ 02-print.sh
@@ -4,24 +4,22 @@ echo Hello Bhai
 
 #print multiple lines
 
-echo -e "Hello Bhai,\n\t Namasthe"
+echo -e "Hello Bhai,\nNamasthe"
 
 #Enabling the colours(\e)
 
 ##Synatx : echo -e "\eColmMessage"
 #COLOURS AND THEIR CODES
 #RED 31
-
 #GREEN 32
-
 #YELLOW 33
-
 #BLUE  34
-
 #MAGENTA 35
-
 #CYAN 36
 
-echo -e "\e[31mMESSAGE IN RED"
+echo -e "\e[31mMESSAGE IN RED\e[0m"
+echo -e "\e[33mMESSAGE IN YELLOW\e[0m"
 
-echo -e "\e[33mMESSAGE IN YELLOW"
+#Colours once enabled will print the other lines in same colour.So we need to disable them once done.
+
+echo "Hello Bhai,em sangathi?"
