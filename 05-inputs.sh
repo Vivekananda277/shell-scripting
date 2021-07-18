22784daf52a88c73802c4c71d834f99c2df44156
Vivek <vivekanandareddy7677@gmail.com>
2021-07-18 21:26:32 +0530

    Variables

--- 05-inputs.sh
+++ 05-inputs.sh
@@ -1,6 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 ##Inputs can be loaded using special variables
 #0-n,*,@,#
 
+#In scripts of we want to access the script name itself we can use $0
 echo $0
+echo $1
