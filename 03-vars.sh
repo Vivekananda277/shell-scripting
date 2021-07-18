d5169ade5b99d0e9522be73457534a9ba5c813ce
Vivek <vivekanandareddy7677@gmail.com>
2021-07-18 20:30:38 +0530

    Variables

--- 03-vars.sh
+++ 03-vars.sh
@@ -6,3 +6,7 @@ echo $A
 ## Command substitutions
 NO_OF_USERS=$(who|wc -l)
 echo -e "\e[33mNumber of users=$NO_OF_USERS\e[0m"
+
+##Date with command substitution
+DATE=$(date +%F)
+echo -e "\e[32mWelcome today's date is $DATE\e[0m"
