8865cd9d876ee80c18be3069f00a628cb79de52d
Vivek <vivekanandareddy7677@gmail.com>
2021-07-18 20:36:58 +0530

    Variables

--- 03-vars.sh
+++ 03-vars.sh
@@ -9,4 +9,4 @@ echo -e "\e[33mNumber of users=$NO_OF_USERS\e[0m"
 
 ##Date with command substitution
 DATE=$(date +%F)
-echo -e "\e[32mWelcome today's date is DATE\e[0m"
+echo -e "\e[35mWelcome today's date is $DATE\e[0m"
