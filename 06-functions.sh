e874577bc5903ac852692d6d87cf7ce36dea648e
Vivek <vivekanandareddy7677@gmail.com>
2021-07-19 12:13:00 +0530

    Sample function

--- 06-functions.sh
+++ 06-functions.sh
@@ -5,8 +5,11 @@
 sample(){
   echo Hello,I am a sample function
   echo -e "\e[34mValue of a is $a\e[0m"
+  b=$(100-30)
 }
+##Main program
 
-##call the function
 a=10
+##call the function
 sample
+echo value of b=$b
