cf80e94b9fe8b9c11c12b2ac54a56644a1b2477c
Vivek <vivekanandareddy7677@gmail.com>
2021-07-24 19:06:00 +0530

    Sample function

--- 06-functions.sh
+++ 06-functions.sh
@@ -6,6 +6,7 @@ sample(){
   echo Hello,I am a sample function
   echo -e "\e[34mValue of a is $a\e[0m"
   b=$((100-30))
+  local c = 130 #scope of this variable is in this sample function
 }
 ##Main program
 
