3866db80b0d6c7271474cf25c41178fb54ab6d97
Vivek <vivekanandareddy7677@gmail.com>
2021-07-25 15:55:50 +0530

    Sample script

--- robotshop/frontend.sh
+++ robotshop/frontend.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 LOG=/tmp/frontend.log
-## Installing nginx
+
+echo -e "\e[31mInstalling Nginx\e[0m"
 yum install nginx -y > $LOG
 ##  1.Output shouldn't be displayed on the terminal
 ##  2.Validate if the command has been executed successfully
