49ed6fbd5d5ef28a1d92fc65ac78ed5e7d7a877d
Vivek <vivekanandareddy7677@gmail.com>
2021-07-26 03:15:15 +0530

    If else

--- robotshop/frontend.sh
+++ robotshop/frontend.sh
@@ -10,17 +10,24 @@ else
   exit 1
 fi
 }
-echo -e -n "Installing Nginx"
-yum install nginx &>> $LOG
+PRINT(){
+  echo -n -e "$1\t\t"
+  }
+
+PRINT "Installing Nginx"
+yum install nginx -y &>> $LOG ## Redirecting the output to $LOG and it is in /tmp/robot shop.log
+STAT_CHECK $?  ##Passing an argument to the STAT_CHECK.So $LOG = $1 in above function
+
+PRINT "Enabling Nginx\t"
+systemctl enable nginx &>> $LOG
 STAT_CHECK $?
+
+PRINT "Start Nginx"
+systemctl start nginx &>> $LOG
+STAT_CHECK $?
+
 ##  1.Output shouldn't be displayed on the terminal
 ##  2.Validate if the command has been executed successfully
 ## 3.Need to validate whether the script is running as a root user
 
-echo -n -e "Enabling Nginx"
-systemctl enable nginx &>> $LOG
-STAT_CHECK $?
-
-echo -e -n "Starting Nginx"
-systemctl start nginx &>> $LOG
-STAT_CHECK $?
+}
