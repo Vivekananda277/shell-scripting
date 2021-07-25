55afbb1bf1dbf591085b4a97210e38312ed33e98
Vivek <vivekanandareddy7677@gmail.com>
2021-07-25 20:00:52 +0530

    Sample script

--- robotshop/frontend.sh
+++ robotshop/frontend.sh
@@ -2,16 +2,29 @@
 
 LOG=/tmp/robotshop.log
 rm -f $LOG
-echo "Installing Nginx"
+echo -e -n "Installing Nginx"
 yum install nginx -y &>> $LOG
-echo $?
+if($?-eq 0);then
+  echo -e "\e[32m done\e[0m"
+else
+  echo Failed
+fi
 ##  1.Output shouldn't be displayed on the terminal
 ##  2.Validate if the command has been executed successfully
 ## 3.Need to validate whether the script is running as a root user
+
 echo "Enabling Nginx"
 systemctl enable nginx &>> $LOG
-echo $?
+if($?-eq 0);then
+  echo -e "\e[32m done\e[0m"
+else
+  echo Failed
+fi
 
 echo "Starting Nginx"
 systemctl start nginx &>> $LOG
-echo $?
+if($>-eq 0);then
+  echo -e "\e[34m done\e[0m"
+else
+  echo Failed
+fi
