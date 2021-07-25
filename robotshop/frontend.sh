bfe5d2a2a32ce385f6665eaad078f9f64a7cc93a
Vivek <vivekanandareddy7677@gmail.com>
2021-07-25 19:36:51 +0530

    Sample script

--- robotshop/frontend.sh
+++ robotshop/frontend.sh
@@ -4,10 +4,14 @@ LOG=/tmp/robotshop.log
 rm -f $LOG
 echo "Installing Nginx"
 yum install nginx -y &>> $LOG
+echo $?
 ##  1.Output shouldn't be displayed on the terminal
 ##  2.Validate if the command has been executed successfully
+## 3.Need to validate whether the script is running as a root user
 echo "Enabling Nginx"
 systemctl enable nginx &>> $LOG
+echo $?
 
 echo "Starting Nginx"
 systemctl start nginx &>> $LOG
+echo $?
