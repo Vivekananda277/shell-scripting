e40fe015434adea144cacddbf04af4fd210147ff
Vivek <vivekanandareddy7677@gmail.com>
2021-07-25 15:52:03 +0530

    Sample script

--- robotshop/frontend.sh
+++ robotshop/frontend.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+LOG=/tmp/frontend.log
 ## Installing nginx
-yum install nginx -y
+yum install nginx -y > $LOG
+##  1.Output shouldn't be displayed on the terminal
+##  2.Validate if the command has been executed successfully
+echo "Enabling Nginx"
 systemctl enable nginx
+
+echo "Starting Nginx"
 systemctl start nginx
