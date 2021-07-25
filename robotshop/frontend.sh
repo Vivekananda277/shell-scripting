b784bee3c667d122381fcf6677180acdab6eec55
Vivek <vivekanandareddy7677@gmail.com>
2021-07-25 20:34:49 +0530

    If else

--- robotshop/frontend.sh
+++ robotshop/frontend.sh
@@ -4,8 +4,8 @@ LOG=/tmp/robotshop.log
 rm -f $LOG
 echo -e -n "Installing Nginx"
 yum install nginx -y &>> $LOG
-if "[ $? -eq 0 ]"; then
-  echo "done"
+if "[ $? -eq 0 ];" ; then
+  echo 'done'
 else
   echo Failed
 fi
